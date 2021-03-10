8614f1950676e104b3f40bf0cd1714153ed308c9
cahangeorge <cahangeorge@gmail.com>
2021-03-10 23:56:10 +0200

    finish the project

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
+
 
 var app = express();
 
@@ -12,6 +14,14 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
